421e0e8caa9bd38b202b69e02ad264d2a41fb72e
CHAIPER <113088435+CHAIPER@users.noreply.github.com>
2023-11-20 02:19:25 -0300

    dialog.cpp

--- EJERCICIO_2_TP2/PANEL_de_CONTROL/dialog.cpp
+++ EJERCICIO_2_TP2/PANEL_de_CONTROL/dialog.cpp
@@ -1,204 +1,200 @@
-#include "dialog.h"
-#include "ui_dialog.h"
-#include <ctime>
-#include <QStringList>
-#include <QBrush>
-#include <QColor>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "wifi.h"
+#include "mqtt_client.h"
+#include <esp_log.h>
+#include "driver/gpio.h"
+#include <math.h>
 
-void Dialog::connected(){
-  ui->list->addItem("Conectado");
-  cliente2.subscribe("/ej02/id");
-  cliente2.subscribe("/ej02/sensor");
+#define TAG "[MQTT]"
+#define min(a, b) (a > b ? b : a)
+static bool mqttOk = false;
+
+#define id 3
+
+#define ledPin GPIO_NUM_2
+#define sensorPin GPIO_NUM_34
+
+void hardware_init(void)
+{
+    gpio_config_t gpioLed =
+        {
+            .pin_bit_mask = 1ULL << ledPin,
+            .mode = GPIO_MODE_DEF_OUTPUT,
+            .pull_up_en = GPIO_PULLUP_DISABLE,
+            .pull_down_en = GPIO_PULLDOWN_DISABLE,
+            .intr_type = GPIO_INTR_DISABLE};
+    ESP_ERROR_CHECK(gpio_config(&gpioLed));
+    ESP_ERROR_CHECK(gpio_set_level(ledPin, 0));
 }
 
-void Dialog::disconnected(){
-  ui->list->addItem("Desconectado");
-}
-void Dialog::error(const QMQTT::ClientError error){
-  ui->list->addItem("Error: " + QString::number(error));
-}
-void Dialog::subscribed(const QString& topic, const quint8 qos){
-  ui->list->addItem("Subscripto:  "  + topic  + "( Qos: " +  QString::number(qos) + ")");
-}
-void Dialog::unsubscribed(const QString& topic){
- ui->list->addItem("Unsubscripto: " + topic);
-}
-void Dialog::published(const QMQTT::Message& message, quint16 msgid){
- ui->list->addItem("Publicado:" + message.topic() + ":" + message.payload()+"MsgId: " +QString::number(msgid));
-}
-void Dialog::pingresp(){
+static esp_mqtt_client_handle_t actClient;
 
-}
-void Dialog::received(const Message &message){
-  Message msg;
-  QStringList datos;
+static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
+{
+    char topic[64];
+    char data[64];
+    ESP_LOGD(TAG, "Event dispatched from event loop base=%s, event_id=%" PRIi32 "", base, event_id);
+    esp_mqtt_event_handle_t event = event_data;
+    esp_mqtt_client_handle_t client = event->client;
+    int msg_id;
 
-  if(message.topic() == "/ej02/sensor"){
-      QString dato = QString(message.payload());
-      datos=dato.split("/",QString::SkipEmptyParts);
-      if(datos.count()==2){
-          if(ui->boxdispositivos->currentText()==datos[1]){
-          actualizarGrafica(datos[0].toDouble());
-      }
-         }
+    switch ((esp_mqtt_event_id_t)event_id)
+    {
+    case MQTT_EVENT_CONNECTED:
+        ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
+        actClient = client;
+
+        // Subscripciones a tópicos relevantes
+        msg_id = esp_mqtt_client_subscribe(client, "/ej02/cmd", 1);
+        ESP_LOGI(TAG, "Subscribe to /ej02/cmd, msg_id=%d", msg_id);
+
+        char idTopicCmd[20];
+        snprintf(idTopicCmd, sizeof(idTopicCmd), "/ej02/%d/cmd", id);
+        msg_id = esp_mqtt_client_subscribe(client, idTopicCmd, 1);
+        ESP_LOGI(TAG, "Subscribe to %s, msg_id=%d", idTopicCmd, msg_id);
+
+        char idTopicSensor[20];
+        snprintf(idTopicSensor, sizeof(idTopicSensor), "/ej02/%d/sensor", id);
+        msg_id = esp_mqtt_client_subscribe(client, idTopicSensor, 1);
+        ESP_LOGI(TAG, "Subscribe to %s, msg_id=%d", idTopicSensor, msg_id);
+
+        mqttOk = true;
+        break;
+
+    case MQTT_EVENT_DISCONNECTED:
+        ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
+        mqttOk = false;
+        break;
+
+    case MQTT_EVENT_SUBSCRIBED:
+        ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
+        break;
+
+    case MQTT_EVENT_UNSUBSCRIBED:
+        ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
+        break;
+
+    case MQTT_EVENT_PUBLISHED:
+        ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
+        break;
+
+    case MQTT_EVENT_DATA:
+        ESP_LOGI(TAG, "MQTT_EVENT_DATA");
+        strncpy(topic, event->topic, min(63, event->topic_len));
+        strncpy(data, event->data, min(63, event->data_len));
+        printf("TOPIC=%s\r\n", topic);
+        printf("DATA=%s\r\n", data);
+
+        if (strcmp(topic, "/ej02/cmd") == 0)
+        {
+            if (strcmp(data, "getid") == 0)
+            {
+                char idTopic[20];
+                snprintf(idTopic, sizeof(idTopic), "/ej02/%d/id", id);
+                msg_id = esp_mqtt_client_publish(client, idTopic, "3", 0, 1, 0);
+                ESP_LOGI(TAG, "Published ID, msg_id=%d", msg_id);
             }
-  if(message.topic()=="/ej02/id"){
-      ui->ListDispo->addItem( QString(message.payload()));
-      ui->boxdispositivos->addItem(QString(message.payload()));
-    }
         }
+        else if (strstr(topic, "/ej02/") != NULL && strstr(topic, "/sensor") != NULL)
+        {
+            // Procesar datos del sensor para el nodo correspondiente
+            // Analizar el ID del nodo desde el tópico
+            char *token = strtok(topic, "/");
+            token = strtok(NULL, "/");
+            int nodeId = atoi(token);
 
-Dialog::Dialog(QWidget *parent)
-    : QDialog(parent)
-    , ui(new Ui::Dialog)
+            // TODO: Procesar datos del sensor para nodeId usando 'data'
+            
+        }
+        else if (strstr(topic, "/ej02/") != NULL && strstr(topic, "/cmd") != NULL)
+        {
+            // Procesar comando para el nodo correspondiente
+            // Analizar el ID del nodo desde el tópico
+            char *token = strtok(topic, "/");
+            token = strtok(NULL, "/");
+            int nodeId = atoi(token);
+
+            if (strcmp(data, "LedOn") == 0 || strcmp(data, "ledOn") == 0)
+            {
+                ESP_LOGI(TAG, "LED ON");
+                ESP_ERROR_CHECK(gpio_set_level(ledPin, 1));
+            }
+            else if (strcmp(data, "LedOff") == 0)
+            {
+                ESP_LOGI(TAG, "LED OFF");
+                ESP_ERROR_CHECK(gpio_set_level(ledPin, 0));
+            }
+        }
+        break;
+
+    case MQTT_EVENT_ERROR:
+        ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
+        ESP_LOGE(TAG, "Error type: %d", event->error_handle->error_type);
+        if (event->error_handle->error_type == MQTT_ERROR_TYPE_TCP_TRANSPORT)
+        {
+            ESP_LOGE(TAG, "Last errno string: %s", strerror(event->error_handle->esp_transport_sock_errno));
+        }
+        break;
+
+    default:
+        ESP_LOGI(TAG, "Other event id:%d", event->event_id);
+        break;
+    }
+}
+
+void taskPublish(void *param)
 {
-    ui->setupUi(this);
+    char toSend[128];
+    float cont = 1;
 
+    while (1)
+    {
+        vTaskDelay(2500 / portTICK_PERIOD_MS);
+        if (mqttOk)
+        {
+            if (cont == 2)
+            {
+                ESP_ERROR_CHECK(gpio_set_level(ledPin, 1));
+            }
+            float senoidal = sin(cont);
+            sprintf(toSend, "%f", senoidal);
 
-    connect(&cliente2, SIGNAL(connected()), this, SLOT(connected()));
-    connect(&cliente2, SIGNAL(disconnected()), this, SLOT(disconnected()));
-    connect(&cliente2, SIGNAL(error(const QMQTT::ClientError)), this, SLOT(error(const QMQTT::ClientError)));
-    connect(&cliente2, SIGNAL(subscribed(const QString&, const quint8)), this, SLOT(subscribed(const QString&, const quint8)));
-    connect(&cliente2, SIGNAL(unsubscribed(const QString&)), this, SLOT(unsubscribed(const QString&)));
-
-    connect(&cliente2, SIGNAL(published(const QMQTT::Message&, quint16 )), this, SLOT(published(const QMQTT::Message&, quint16 )));
-    connect(&cliente2, SIGNAL(pingresp()), this, SLOT(pingresp()));
-    connect(&cliente2, SIGNAL(received(const QMQTT::Message&)), this, SLOT(received(const QMQTT::Message&)));
-
-    ui->grafico->addGraph();
-    ui->grafico->xAxis->setLabel("Tiempo");
-    ui->grafico->yAxis->setLabel("Valores del sensor");
-    tiempo = 0;
-   ui->grafico->xAxis->setRange(0,1000);
-   ui->grafico->yAxis->setRange(0,100);
+            char idTopic[20];
+            snprintf(idTopic, sizeof(idTopic), "/ej02/%d/sensor", id);
+            esp_mqtt_client_publish(actClient, idTopic, toSend, 0, 2, 0);
+            cont = cont + (0.2);
+        }
+    }
 }
 
-Dialog::~Dialog()
+void app_main(void)
 {
-    delete ui;
-}
+    esp_mqtt_client_config_t mqtt_cfg = {
+        .broker.address.uri = "mqtt://192.168.0.12",
+        .credentials.username = "esp32",
+        .credentials.authentication.password = "esp32esp32",
+    };
 
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        nvs_flash_init();
+    }
 
-void Dialog::on_btn_connect_clicked()
-{
-    ui->list->clear();
-    cliente2.setHostName(ui->le_servidor->text());
-    if((ui->le_servidor->text().length() > 0) && (ui->le_clave->text().length() > 0)){
-        cliente2.setUsername(ui->le_servidor->text());
-        cliente2.setPassword(ui->le_clave->text().toLatin1());
-      }
-    cliente2.setPort(ui->le_puerto->text().toInt());
-    cliente2.connectToHost();
+    hardware_init();
+    wifi_init_sta();
 
-
-}
-
-void Dialog::on_btn_desconectar_clicked()
-{
-    if(cliente2.isConnectedToHost()){
-       cliente2.disconnectFromHost();
-      }
-    ui->list->clear();
-    ui->ListDispo->clear();
-    ui->grafico->removeGraph(0);
-    ui->grafico->addGraph();
-    ui->grafico->replot();
-}
-
-
-void Dialog::on_btn_led_on_clicked()
-{
-
-QMQTT ::Message ledon;
-QList<QListWidgetItem*> selected;
-QList<QListWidgetItem*> :: iterator item;
-selected = ui->ListDispo->selectedItems();
-for(item = selected.begin(); item != selected.end(); item++){
-    ledon.setTopic("/ej02/"+(*item)->text()+"/cmd");
-    ledon.setPayload("ledon");
-    cliente2.publish(ledon);
-}
-
-}
-
-void Dialog::on_btn_GETID_clicked()
-{
-    ui->list->clear();
-
-    QMQTT ::Message mensaje;
-    mensaje.setTopic("/ej02/cmd");
-    mensaje.setPayload("getid");
-    cliente2.publish(mensaje);
-    ui->boxdispositivos->clear();
-    ui->list->clear();
-    ui->ListDispo->clear();
-    ui->grafico->removeGraph(0);
-    ui->grafico->addGraph();
-    ui->grafico->replot();
-    ui->grafico->clearGraphs();
-
-}
-
-void Dialog::on_btn_led_Off_clicked()
-{
-    QMQTT ::Message ledoff;
-    QList<QListWidgetItem*> selected;
-    QList<QListWidgetItem*> :: iterator item;
-    selected = ui->ListDispo->selectedItems();
-    for(item = selected.begin(); item != selected.end(); item++){
-        ledoff.setTopic("/ej02/"+(*item)->text()+"/cmd");
-        ledoff.setPayload("ledoff");
-        cliente2.publish(ledoff);
-}
-}
-
-
-void Dialog::on_pushButton_clicked()
-{
-    QApplication ::exit();
-}
-
-void Dialog :: actualizarGrafica(double val) {
-
-    QCPGraph *graph = ui->grafico->graph(0);
-
-
-    graph->addData(tiempo,val); // Añadir punto al gráfico
-    tiempo++;
-
-    // Ajustar el rango del eje X para mostrar solo un intervalo de tiempo
-    ui->grafico->xAxis->setRange(tiempo - 1, tiempo);  // Mostrar el último segundo
-
-    // acomodar el eje Y para adaptarse a los nuevos datos
-    ui->grafico->rescaleAxes();
-
-     double Rangoinferior = tiempo - 1;
-     int eliminarcont = 0;
-     while (graph->data()->at(eliminarcont)->key < Rangoinferior) {
-         eliminarcont++;
-     }
-     graph->data()->removeBefore(eliminarcont);
-     // Crear un nuevo gráfico para el área debajo de la curva
-       QCPGraph *fillGraph = ui->grafico->addGraph();
-       fillGraph->setPen(Qt::NoPen); // Sin línea de borde
-       fillGraph->setBrush(QBrush(QColor(81, 209, 246, 10))); // Color Celeste con opacidad 90
-
-       // Añadir datos al gráfico de relleno para crear el área sombreada
-       for (int i = 0; i < graph->data()->size(); ++i) {
-           fillGraph->addData(graph->data()->at(i)->key, graph->data()->at(i)->value);
-       }
-
-
-
-    ui->grafico->replot(); // Redibujar el gráfico
-}
-
-
-
-void Dialog::on_boxdispositivos_currentIndexChanged(const QString &arg1)
-{
-    ui->grafico->removeGraph(0);
-    ui->grafico->addGraph();
-    ui->grafico->replot();
-    ui->list->clear();
+    esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
+    esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
+    esp_mqtt_client_start(client);
+    xTaskCreate(taskPublish, "Publish Task", configMINIMAL_STACK_SIZE + 4096, NULL, 1, NULL);
 }
